b20d854f04fa8a085036b540d83e39f5d86767d9
John van Dijk <fortyfour-three@teksavvy.com>
2019-12-18 21:41:02 -0500

    Working to download data, consolidate it, and filter it by Christmas day. The final chart is made in a spreadsheet based on the Christmas Day snow amounts on the ground, or that have just fallen.

--- weather_data.py
+++ weather_data.py
@@ -14,6 +14,7 @@ import csv
 start_year 	= 1972
 end_year 	= 2019
 weather_station = "4816" 		## Make sure this is in quotes.
+## weather_station = "48569"	## Kitchener/ Waterloo Station that starts in 2010
 
 def download_data(start_year, end_year, weather_station):
 	for year in range (start_year, end_year+1):
@@ -29,19 +30,62 @@ def download_data(start_year, end_year, weather_station):
 def clean_data(start_year, end_year):
 	## With Thanks to Martijn Pieters https://stackoverflow.com/users/100297/martijn-pieters
 	## https://stackoverflow.com/questions/14257373/skip-the-headers-when-editing-a-csv-file-using-python
+
+	## Reset the data file.
+	f = open("data.csv", "w")
+	f.close()
+		
+	flag = 1					## If the flag = 1 then headers are added, otherwise they are not.
+
+	for data_file in range(start_year, end_year + 1):
+		fn = str(data_file) + ".csv"	
+		with open(fn, "r") as infile, open("data.csv", "a", newline='\n') as outfile:
+		   reader = csv.reader(infile)
+		   
+		   if flag == 0: 
+			   next(reader, None)  	## skip the headers
+			
+		   flag = 0					## Set the flag to 0. You only need one header.
+			   
+		   writer = csv.writer(outfile)
+		   for row in reader:
+			   # process each row
+			   writer.writerow(row)
+		
+		print ("Processing File . . ."+fn)
 	
-	flag = 1		## If the flag = 1 then headers are added, otherwise they are not.
+	print ("\nData saved to  . . . data.csv")
+	infile.close()
+		
+
+	 
+def get_christmas_row(start_year, end_year):
+	## Reset the data file.
+	f = open("christmas-data.csv", "w")
+	f.close()
 	
-	# for year in range
-	# after the first year, set the flag to 0.	
-	with open("2017.csv", "r") as infile, open("data.csv", "a", newline='\n') as outfile:
-	   reader = csv.reader(infile)
-	   if flag != 1: 
-		   next(reader, None)  # skip the headers
-	   writer = csv.writer(outfile)
-	   for row in reader:
-		   # process each row
-		   writer.writerow(row)
+	with open("data.csv", "r") as infile, open("christmas-data.csv", "w", newline = "\n") as outfile: 
+		reader = csv.reader(infile)
+		writer = csv.writer(outfile)
+		
+		## Add the header to the new file.
+		header = next(reader)
+		writer.writerow(header)
+		
+		## Add all of the other rows.
+		for row in reader:
+			if row[6] == "12" and row[7] == "24":
+				writer.writerow(row)
        
-#download_data(start_year, end_year)
+	print ("\nData saved to  . . . christmas-data.csv")
+	
+
+## Download the data	
+download_data(start_year, end_year)
+
+## Clean it and put it into one spreadsheet.
 clean_data(start_year, end_year)
+
+## Filter the consolidated sheet by December 24th.
+get_christmas_row(start_year, end_year)
+
